fdc5748032828d46367da8fabbd3797ba8844da7
mdnazisharman2803 <md.arman.e21@nsut.ac.in>
2022-04-25 00:12:09 +0530

    Third Maximum Number

---
+++ Day-14/ThirdMaximumNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+       
+       sort( nums.begin(), nums.end() );
+        nums.erase( unique( nums.begin(), nums.end() ), nums.end() );
+        
+      
+        if(nums.size()>=3)
+        {
+            return nums[nums.size()-3];
+        }
+        
+        return nums[nums.size()-1];
+        
+        
+    }
+};
