50ed115232792ef9373e166e599c628658e6693b
mdnazisharman2803 <md.arman.e21@nsut.ac.in>
2022-04-25 00:14:43 +0530

    Number of Good Pairs

---
+++ Day-14/NumberofGoodPairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j] && i<j)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
